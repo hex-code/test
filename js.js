5d71b36df72f886c37c73aa789f76e2f919279d2
hex-code <GrozaRazor@gmail.com>
2014-06-16 23:30:22 +0300

    Simply function that consist from 2 circles. Small circle whirling around big circle

---
+++ js.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	var angle = 0,
+	increase = (2 * Math.PI) / 20;
+
+	function rotate () {
+	x = 0,
+	y = 0;
+	for (var i = 0; i < 20; i++) {
+		var c = document.getElementById("circle"),
+		x = 200 * Math.cos( angle );
+		y = 200 * Math.sin( angle );
+		angle += increase;
+		c.style.left = x + "px";
+		c.style.top = y + "px";
+		return c
+	};
+};
+	setInterval (rotate, 40);
+});
