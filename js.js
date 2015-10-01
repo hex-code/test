f120566aa669071c96a4a7e9cad11e3185ea6d46
Yaroslav Sava <GrozaRazor@gmail.com>
2015-10-01 14:38:28 +0300

    modify code

--- js.js
+++ js.js
@@ -2,16 +2,16 @@
 	var angle = 0,
 	increase = (2 * Math.PI) / 20;
 
-	function rotate () {
-	var x = 0,
-	    y = 0,
-        c = document.getElementById("circle");
-	x = 200 * Math.cos( angle );
-	y = 200 * Math.sin( angle );
-	angle += increase;
-	c.style.left = x + "px";
-	c.style.top = y + "px";
-	return c
-};
+    function rotate () {
+	   var x = 0,
+           y = 0,
+            c = document.getElementById("circle");
+	   x = 200 * Math.cos( angle );
+	   y = 200 * Math.sin( angle );
+	   angle += increase;
+	   c.style.left = x + "px";
+	   c.style.top = y + "px";
+	   return c
+    };
 	setInterval (rotate, 40);
 })();
